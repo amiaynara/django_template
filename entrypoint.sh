0dce1980fbda06ec0fb49f763eaabd6028c002aa
Amiay Narayan <amiayn@iitbhilai.ac.in>
2025-04-26 15:23:21 +0530

    Feature: Add postgres database to replace sqlite

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo "running entrypoint.sh"
+# database actions
+/home/appuser/manage.py makemigrations
+/home/appuser/manage.py migrate
 
 # run the backend server 
 python manage.py runserver 0.0.0.0:8000
