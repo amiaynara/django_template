f60f22a683add78e37f53b6ca7b0dec20564e574
Amiay Narayan <amiayn@iitbhilai.ac.in>
2025-04-26 18:56:58 +0530

    Feature: Add NGINX reverse proxy to the gunicorn http server

--- template_app/entrypoint.local.sh
+++ template_app/entrypoint.local.sh
@@ -4,5 +4,8 @@
 /home/appuser/manage.py makemigrations
 /home/appuser/manage.py migrate
 
+# collect static files
+/home/appuser/manage.py collectstatic --noinput
+
 # run the backend server 
 python manage.py runserver 0.0.0.0:8000
--- template_app/entrypoint.sh
+++ template_app/entrypoint.sh
@@ -4,6 +4,9 @@
 /home/appuser/manage.py makemigrations
 /home/appuser/manage.py migrate
 
+# collect static files
+/home/appuser/manage.py collectstatic --noinput
+
 # Start Gunicorn server
 echo "Starting Gunicorn server..."
 gunicorn template_app.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 120
